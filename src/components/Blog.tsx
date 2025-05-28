89aff0fc44bf6cae1481ff0b77576d7bd0cc5132
Linushankaran18 <linushankaranjana@gmail.com>
2025-05-28 17:11:08 +0530

    bloggg

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -104,7 +104,7 @@ const Blog = () => {
         
         <div className="text-center mt-10">
           <Link 
-            to="/blog"
+            to="/blogs"
             className="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 transition duration-300"
           >
             View All Articles
